23b0019d0f42c909c79bd74c0760379ebc5355c1
Dawson Tilley <16dit@queenus.ca>
2023-10-24 20:18:07 -0400

    changed chrome driver env and cleanup comments

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.chrome.options import Options
 import datetime
 import time
+from os import environ
+
 
 
 def three_days_out():
@@ -14,9 +18,6 @@ def book_ideal_time():
     ans = "1"
     return ans
 
-#url = "https://northwestbadmintonacademy.sites.zenplanner.com/login.cfm"
-#url = "https://auth.buildinglink.com/Account/Login"
-#url = https://thepinnaclecentre.buildinglink.com/v2/tenant/amenities/NewReservation.aspx?from=1&amenityId=16844&starts=9/22/2023%205:00:00%20PM&ends=9/22/2023%206:00:00%20PM
 url = "Http://www.thepinnaclecentre.com"
 
 amenity_id = 16844
@@ -24,13 +25,14 @@ start = "9/22/2023%205:00:00%20PM&"
 end = "9/22/2023%206:00:00%20PM"
 #browser = webdriver.Chrome(executable_path='./chromedriver.exe')
 #browser.get(url)
-
-service = Service(executable_path='./chromedriver.exe')
-options = webdriver.ChromeOptions()
+print(environ.get("CHROMEWEBDRIVER",'./chromedriver.exe'))
+#options = Options
 #options.add_argument('--no-sandbox')
 #options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=service, options=options)
+#options.add_argument("--headless=new")
+service = Service(executable_path= environ.get("CHROMEWEBDRIVER",'./chromedriver.exe'))
+
+driver = webdriver.Chrome(service=service)
 driver.implicitly_wait(2)
 
 driver.get(url)
@@ -51,74 +53,25 @@ amenity_res.click()
 book_res =  driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_NewReservatrionButton']")
 book_res.click()
 
-squash_pick = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_AmenitiesDataList_ctl18_SelectAmenityLink']")
+#squash_pick = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_AmenitiesDataList_ctl18_SelectAmenityLink']")
+squash_pick = driver.find_element("xpath", "//a[text() = 'Squash Court']")
 squash_pick.click()
 
-
-
-
-
-""""
-grid_res = driver.find_element("xpath","//*[@id='ctl00_ContentPlaceHolder1_AvailabilityGridViewButton']")
-grid_res.click()
-
-fake_input_for_squash = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_AmenitiesDropdownTree']/span/span[1]")
-fake_input_for_squash.click()
-
-squash_selection = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_AmenitiesDropdownTree_EmbeddedTree']/ul/li[16]/div/span[2]")
-squash_selection.click()
-
-next_day = driver.find_element("xpath","//*[@id='ctl00_ContentPlaceHolder1_ButtonNextDay']")
-next_day.click()
-next_day.click()
-
-
-refresh = driver.find_element("xpath","//*[@id='ctl00_ContentPlaceHolder1_RefreshCalendarButton']")
-refresh.click()
-"""
 
 date_to_book = three_days_out()
 print(date_to_book)
 xpath_booking_date = f"//a[text()={date_to_book}]"
 
-
-#xpath_booking_date = driver.find_element_by_xpath("//*[@id='ctl00_ContentPlaceHolder1_StartDatePicker_Top']/tbody/tr[5]/td[2]/a")
 booking_date = driver.find_element("xpath",xpath_booking_date)
 booking_date.click()
-
-#time_box = driver.find_element("xpath" , "//*[@id='ctl00_ContentPlaceHolder1_StartTimePicker_dateInput']")
-#time_box.click()
-
-#six_pm_time = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_StartTimePicker_timeView_tdl']/tbody/tr[4]/td[2]/a")
-#seven_pm_time = driver.find_element("xpath","//*[@id='ctl00_ContentPlaceHolder1_StartTimePicker_timeView_tdl;]/tbody/tr[4]/td[3]/a")
-#seven_pm_time.click()
-#//*[@id="ctl00_ContentPlaceHolder1_StartTimePicker_timeView_tdl"]/tbody/tr[3]/td[2]/a
-#xpath_time = "//tbody/tr/td/a/[text()=7:00 PM]"
-
-#start_time = driver.find_element("xpath", "//[@id'ctl00_ContentPlaceHolder1_StartTimePicker_dateInput_wrapper']")
-#start_time.click()
-
-
-#maybe_xpath = "//tbody/tr/td/a[text()='7:00 PM']"
-#xpath = "//a[text() ='7:00 PM']"
-#time_to_book = driver.find_element("xpath" , "(//a[contains(text(), '7:00 pm')])[1])")
-#time_to_book = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_StartTimePicker_dateInput_wrapper']")
-#time_to_book.click()
 
 time.sleep(2)
 
 time_to_book = driver.find_element("xpath", "//*[@id='ctl00_ContentPlaceHolder1_DateSelectionPanel']/ul/li[2]/div/div[1]/div[4]")
 time_to_book.click()
-#"//input[@id='ctl00_ContentPlaceHolder1_StartTimePicker_wrapper']"
-
-#time.sleep(2)
-
-#time_block = driver.find_element("xpath", //*[@id="ctl00_ContentPlaceHolder1_StartTimePicker_dateInput"])
-#//*[@id='ctl00_ContentPlaceHolder1_StartTimePicker_timeView_tdl']/tr/td
 
 book_time = driver.find_element("xpath", "(//tbody/tr/td/a[text() = '8:00 PM'])[1]")
 book_time.click()
-#//*[@id="ctl00_ContentPlaceHolder1_StartTimePicker_timeView_tdl"]/tbody/tr[4]/td[3]/a
 
 time.sleep(2)
 save_reso = driver.find_element("xpath" , "//*[@id='ctl00_ContentPlaceHolder1_FooterSaveButton']")
